9763518100f50c641338c1ce5a896a7422f05c60
Jack Pflaum <jackpflaum@gmail.com>
2024-04-05 17:28:58 +0800

    created client and job routes

---
+++ node-backend/src/routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { ClientController } from '../controllers/ClientController'
+import { JobController } from '../controllers/JobController';
+
+const router = express.Router();
+
+router.get('/clients', ClientController.getClients);
+router.get('/clients/:id', ClientController.getClientDetails);
+router.post('/clients', ClientController.addNewClient);
+router.put('/clients/:id', ClientController.updateClientDetails);
+
+router.get('/jobs', JobController.getJobs);
+router.get('/jobs/:id', JobController.getJobDetails);
+router.post('/jobs', JobController.addNewJob);
+router.put('/jobs/:id', JobController.updateJobDetails);
+
+export default router;
