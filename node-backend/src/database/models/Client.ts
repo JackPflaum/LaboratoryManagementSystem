ccc1fcd5efe0755c78bcb931e0c42cb899e4357a
Jack Pflaum <jackpflaum@gmail.com>
2024-04-05 17:27:03 +0800

    created Client model

---
+++ node-backend/src/database/models/Client.ts
@@ -0,0 +1,69 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../models/db';    // import database instance from database.ts
+
+
+interface ClientAttributes {
+    id: number;
+    name: string;
+    email: string;
+    phoneNumber?: string;
+    address?: string;
+    purchaseOrderNumber?: string;
+}
+
+// In Typescript '!' means the attribute must be assigned a value and '?' means it is optional
+class Client extends Model<ClientAttributes> implements ClientAttributes {
+    id!: number;
+    name!: string;
+    email!: string;
+    phoneNumber?: string;
+    address?: string;
+    purchaseOrderNumber?: string;
+
+    // timestamps
+    readonly createdAt!: Date;
+    readonly updatedAt!: Date;
+
+
+    static associate(models: any) {
+        Client.hasMany(models.Job, {
+           foreignKey: 'clientId',
+        });
+    }
+}
+
+
+Client.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    phoneNumber: {
+        type: DataTypes.STRING,
+    },
+    address: {
+        type: DataTypes.STRING,
+    },
+    purchaseOrderNumber: {
+        type: DataTypes.STRING,
+        unique: true,
+    }
+}, {
+    sequelize,    // Pass the sequelized instance. It tells which database instance to use for that model.
+    modelName: 'Client',
+    tableName: 'clients',
+});
+
+export default Client;
