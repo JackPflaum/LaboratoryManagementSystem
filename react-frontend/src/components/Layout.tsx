6c272419bcc2720a8266bdf4cd2fb19a72a64be9
Jack Pflaum <jackpflaum@gmail.com>
2024-04-08 20:16:38 +0800

    created Layout page for implementing components

---
+++ react-frontend/src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from './Navigation';
+import { Outlet } from 'react-router-dom';
+
+
+const Layout: React.FunctionComponent = () => {
+    return (
+        <>
+            <Navigation />
+            <Outlet />
+        </>
+    );
+};
+
+
+export default Layout;
