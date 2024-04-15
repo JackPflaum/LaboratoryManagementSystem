2b315b10c214a49817b9015a5cce42f0e495e7eb
Jack Pflaum <jackpflaum@gmail.com>
2024-04-15 20:12:06 +0800

    added CORS to backend

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import routes from '../routes';
+import routes from './routes';
+import cors from 'cors';
+
 
 // load up '.env' file into the 'process.env' object of Node.js
 dotenv.config();
@@ -8,10 +10,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+// use CORS (Cross-Origin Resource Sharing) middleware package for secure requests between front and backend.
+app.use(cors({ origin: 'http://localhost:8000'}));    // allowing requests from this origin to access backend resources
+
 // converts incoming JSON payloads in request bodies to javascript object which becomes available in request.body object
 app.use(express.json());
 
-app.use('api/', routes);
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`Listen for server running on http://localhost:${port}`);
