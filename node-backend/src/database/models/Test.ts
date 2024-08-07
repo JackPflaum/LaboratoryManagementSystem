0f0bf7577a8e550bb83119c7beffcbe791e369b2
Jack Pflaum <jackpflaum@gmail.com>
2024-08-07 15:38:38 +0800

    created Test model

---
+++ node-backend/src/database/models/Test.ts
@@ -0,0 +1,72 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from './db';    // import database instance from database.ts
+import Sample from '../models/Sample';
+import Profile from '../models/Profile';
+import { ModelsInterface } from '../types/models-interface';
+
+
+interface TestAttributes {
+    sampleId: number;
+    profileId: number;
+    testName: string;
+    unit: string;
+    result?: number;
+    comment?: string;
+}
+
+
+class Test extends Model<TestAttributes> implements TestAttributes {
+    sampleId!: number;
+    profileId!: number;
+    testName!: string;
+    unit!: string;
+    result?: number;
+    comment?: string;
+
+    static associate(models: ModelsInterface) {
+        Test.belongsTo(models.Sample, { foreignKey: 'sampleId', onDelete: 'CASCADE' });
+        Test.belongsTo(models.Profile, { foreignKey: 'profileId' });
+    }
+}
+
+
+Test.init({
+    sampleId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Sample,
+            key: 'id',
+        }
+    },
+    profileId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Profile,
+            key: 'id',
+        },
+    },
+    testName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    unit: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    result: {
+        type: DataTypes.INTEGER,
+    },
+    comment: {
+        type: DataTypes.STRING,
+    },
+}, {
+    sequelize,
+    modelName: 'Test',
+    tableName: 'tests',
+    timestamps: true,
+});
+
+
+export default Test;
