0337b7c9ae26e3efcc622626eace76a0e0fce708
Jack Pflaum <jackpflaum@gmail.com>
2024-08-08 16:56:00 +0800

    moved model interfaces into dedicated file

--- node-backend/src/database/types/models-interface.ts
+++ node-backend/src/database/types/models-interface.ts
@@ -15,3 +15,64 @@ export interface ModelsInterface {
     Test: typeof Test;
     User: typeof User;
 }
+
+export interface ClientAttributes {
+    id: number;
+    name: string;
+    email: string;
+    phoneNumber?: string;
+    address?: string;
+    purchaseOrderNumber?: string;
+}
+
+export interface JobAttributes {
+    id: number;
+    clientId: number;
+    jobNumber: string;
+    comments?: string;
+    dueDate: Date;
+    completed?: boolean;
+}
+
+export interface UserAtributes {
+    id: number;
+    firstName: string;
+    lastName: string;
+    workEmail: string;
+    password: string;
+}
+
+export interface ProfileAttributes {
+    id: number;
+    userId: number;
+    personalEmail?: string;
+    phoneNumber?: string;
+    position?: string;
+    dateStarted: Date;
+}
+
+export interface SampleAttributes {
+    id: number;
+    jobNumber: string;
+    sampleNumber: string;
+    type: string;
+    storage: string;
+    completed: boolean;
+    comments?: string;
+}
+
+export interface SamplePhotoAttributes {
+    id: number;
+    sampleId: number;
+    photo: Buffer;    // stored as binary data
+}
+
+export interface TestAttributes {
+    id: number;
+    sampleId: number;
+    profileId: number;
+    testName: string;
+    unit: string;
+    result?: number;
+    comment?: string;
+}
