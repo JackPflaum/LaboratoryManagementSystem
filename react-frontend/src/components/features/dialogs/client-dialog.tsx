1942949791fbb41554e6d7c094bae3dfc133d300
Jack Pflaum <jackpflaum@gmail.com>
2024-08-14 18:26:37 +0800

    added react-hook-form input and yup validation

--- react-frontend/src/components/features/dialogs/client-dialog.tsx
+++ react-frontend/src/components/features/dialogs/client-dialog.tsx
@@ -1,27 +1,213 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material"
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Grid, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material"
+import { Controller, useForm } from "react-hook-form";
+import { ClientAttributes } from "../../../types/interfaces";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
 
-interface ClientsDialogProps {
-    data?: string;
+// client details validation schema
+const clientSchema = yup.object().shape({
+    clientName: yup.string().trim().required("Client name is required"),
+    email: yup.string().trim().matches(/\S+@\S+\.\S/, "Invalid email address").required("Email is required"),
+    phoneNumber: yup.string().trim().optional(),
+    addressLine: yup.string().optional(),
+    suburb: yup.string().optional(),
+    state: yup.string().optional(),
+    postcode: yup.string().optional(),
+    purchaseOrderNumber: yup.string().trim().optional(),
+});
+
+interface ClientDialogProps {
+    data?: ClientAttributes;
     open: boolean;
     handleClose: () => void;
 }
 
-const ClientDialog = ({ data, open, handleClose }: ClientsDialogProps) => {
+const ClientDialog = ({ data, open, handleClose }: ClientDialogProps) => {
+
+    const australianStates = [
+        { value: "", label: "Select State" },
+        { value: "NSW", label: "New South Wales" },
+        { value: "VIC", label: "Victoria" },
+        { value: "QLD", label: "Queensland" },
+        { value: "SA", label: "South Australia" },
+        { value: "WA", label: "Western Australia" },
+        { value: "TAS", label: "Tasmania" },
+        { value: "NT", label: "Northern Territory" },
+        { value: "ACT", label: "Australian Capital Territory" }
+    ];
+
+    const mapDataToForm = (data?: ClientAttributes) => {
+        return {
+            clientName: data?.name ?? "",
+            email: data?.email ?? "",
+            phoneNumber: data?.phoneNumber ?? "",
+            addressLine: data?.addressLine ?? "",
+            suburb: data?.suburb ?? "",
+            state: data?.state ?? "",
+            postcode: data?.postcode ?? "",
+            purchaseOrderNumber: data?.purchaseOrderNumber ?? "",
+        }
+    };
+
+    const { handleSubmit, control, formState: { errors } } = useForm({
+        defaultValues: mapDataToForm(data),
+        resolver: yupResolver(clientSchema)
+    });
+
+    // handle client form submission
+    const onSubmit = (formData: ClientAttributes) => {
+        console.log("Data", formData);
+    };
+
+
     return (
         <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
             <DialogTitle>{!data ? "Add Client" : "Edit Client"}</DialogTitle>
             <DialogContent>
-                {/* Add Form input here */}
+                <Stack spacing={2} sx={{ marginTop: 2 }}>
+                    <Controller
+                        name="clientName"
+                        control={control}
+                        defaultValue={data?.name}
+                        render={({ field, fieldState }) => (
+                            <TextField
+                                label="Name"
+                                value={field.value}
+                                onChange={(e) => {
+                                    field.onChange(e)
+                                }}
+                                error={!!fieldState.error}
+                                helperText={fieldState.error?.message}
+                                size="small"
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="email"
+                        control={control}
+                        defaultValue={data?.email}
+                        render={({ field, fieldState }) => (
+                            <TextField
+                                label="Email"
+                                value={field.value}
+                                onChange={(e) => {
+                                    field.onChange(e);
+                                }}
+                                error={!!fieldState.error}
+                                helperText={fieldState.error?.message}
+                                size="small"
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="phoneNumber"
+                        control={control}
+                        defaultValue={data?.phoneNumber}
+                        render={({ field, fieldState }) => (
+                            <TextField
+                                label="Phone Number"
+                                value={field.value}
+                                onChange={(e) => {
+                                    field.onChange(e);
+                                }}
+                                error={!!fieldState.error}
+                                helperText={fieldState.error?.message}
+                                size="small"
+                            />
+                        )}
+                    />
+                    <Grid container>
+                        <Grid item xs={12} md={6} >
+                            <Stack marginRight={2}>
+                                <Controller
+                                    name="addressLine"
+                                    control={control}
+                                    defaultValue={data?.addressLine}
+                                    render={({ field, fieldState }) => (
+                                        <TextField
+                                            label="Address Line"
+                                            value={field.value}
+                                            onChange={(e) => {
+                                                field.onChange(e);
+                                            }}
+                                            error={!!fieldState.error}
+                                            helperText={fieldState.error?.message}
+                                            size="small"
+                                        />
+                                    )}
+                                />
+                                <Controller
+                                    name="suburb"
+                                    control={control}
+                                    defaultValue={data?.suburb}
+                                    render={({ field, fieldState }) => (
+                                        <TextField
+                                            label="Suburb"
+                                            value={field.value}
+                                            onChange={(e) => {
+                                                field.onChange(e);
+                                            }}
+                                            error={!!fieldState.error}
+                                            helperText={fieldState.error?.message}
+                                            size="small"
+                                        />
+                                    )}
+                                />
+                            </Stack>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Controller
+                                name="state"
+                                control={control}
+                                defaultValue={data?.state || ""}
+                                render={({ field, fieldState }) => (
+                                    <FormControl size="small">
+                                        <InputLabel>State</InputLabel>
+                                        <Select
+                                            label="State"
+                                            {...field}
+                                            error={!!fieldState.error}
+                                            placeholder={fieldState.error?.message}
+                                        >
+                                            {australianStates.map((state) => (
+                                                <MenuItem key={state.value} value={state.value}>
+                                                    {state.label}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                )}
+                            />
+                            <Controller
+                                name="postcode"
+                                control={control}
+                                defaultValue={data?.postcode}
+                                render={({ field, fieldState }) => (
+                                    <TextField
+                                        label="Postcode"
+                                        value={field.value}
+                                        onChange={(e) => {
+                                            field.onChange(e);
+                                        }}
+                                        error={!!fieldState.error}
+                                        helperText={fieldState.error?.message}
+                                        size="small"
+                                    />
+                                )}
+                            />
+                        </Grid>
+                    </Grid>
+                </Stack>
             </DialogContent>
             <DialogActions>
                 <Button variant="contained" onClick={handleClose}>
                     Cancel
                 </Button>
-                <Button variant="contained">
+                <Button variant="contained" onClick={handleSubmit(onSubmit)}>
                     Save
                 </Button>
             </DialogActions>
-        </Dialog>
+        </Dialog >
     );
 };
 
