bd47e1e89658dce5e075c610065049be8e730ba8
Jack Pflaum <jackpflaum@gmail.com>
2024-08-28 17:20:08 +0800

    added "no rows" custom display

--- react-frontend/src/components/features/display-grid.tsx
+++ react-frontend/src/components/features/display-grid.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material"
 import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid"
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 
 interface DisplayGridProps {
     columns: GridColDef[];
@@ -11,7 +12,7 @@ const DisplayGrid = ({ columns, rows, isLoading }: DisplayGridProps) => {
     const pageSize = 5;
 
     return (
-        <Box>
+        <Box sx={{ width: "100%", height: 340 }}>
             <DataGrid
                 rows={rows}
                 columns={columns}
@@ -28,6 +29,21 @@ const DisplayGrid = ({ columns, rows, isLoading }: DisplayGridProps) => {
                     borderRadius: 4,
                 }}
                 loading={isLoading}
+                slots={{
+                    noRowsOverlay: () => (
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                height: '100%',
+                                color: "grey",
+                            }}
+                        >
+                            <WarningAmberIcon />No Rows
+                        </Box>
+                    ),
+                }}
             />
         </Box>
     )
