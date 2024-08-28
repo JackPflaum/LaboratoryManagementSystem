feda14e2f05307f53c2d0cf763573e4a9c766012
Jack Pflaum <jackpflaum@gmail.com>
2024-08-28 17:20:22 +0800

    fixed actions in client columns

--- react-frontend/src/components/features/grid-columns/clients-column.tsx
+++ react-frontend/src/components/features/grid-columns/clients-column.tsx
@@ -1,68 +1,90 @@
-import { GridActionsCellItem, GridColDef, GridRowId, GridRowParams } from "@mui/x-data-grid";
+import { GridActionsCellItem, GridColDef, GridRowParams } from "@mui/x-data-grid";
 import { ClientAttributes } from "../../../types/interfaces";
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 interface ClientColumnProps {
+    viewAction: (row: ClientAttributes) => void;
     editAction?: (row: ClientAttributes) => void;
     deleteAction?: (row: ClientAttributes) => void;
-}
-
+};
 
 // TODO: change to useMemo()
 
-export function getClientsColumns({ editAction, deleteAction }: ClientColumnProps) {
+export function getClientsColumns({ viewAction, editAction, deleteAction }: ClientColumnProps) {
+
     const columns: GridColDef[] = [
         {
             field: "name",
             headerName: "Client Name",
-            width: 150
+            width: 200
         },
         {
             field: "email",
             headerName: "Email",
-            width: 150
+            width: 200
         },
         {
             field: "phoneNumber",
             headerName: "Phone number",
-            width: 150
+            width: 200
         },
         {
             field: "fullAddress",
             headerName: "Address",
-            width: 150,
+            width: 200,
         },
         {
             field: "purchaseOrderNumber",
             headerName: "Purchase Order Number",
-            width: 150
+            width: 200
         }
     ];
 
-
-    if (editAction && deleteAction) {
-        columns.unshift({
-            field: 'actions',
-            type: "actions",
-            width: 100,
-            getActions: (params: GridRowParams<ClientAttributes>) => [
+    // Conditional actions column
+    const actionsColumn: GridColDef = {
+        field: 'actions',
+        type: "actions",
+        width: 100,
+        getActions: (params: GridRowParams<ClientAttributes>) => {
+            const actions = [
                 <GridActionsCellItem
-                    key="edit"
-                    label="Edit"
-                    icon={<EditIcon />}
-                    onClick={() => editAction(params.row)}
-                    showInMenu
-                />,
-                <GridActionsCellItem
-                    key="delete"
-                    label="Delete"
-                    icon={<DeleteIcon />}
-                    onClick={() => deleteAction(params.row)}
+                    key="view"
+                    label="View"
+                    icon={<VisibilityIcon />}
+                    onClick={() => viewAction(params.row)}
                     showInMenu
                 />
-            ]
-        });
-    }
+            ];
+
+            if (editAction) {
+                actions.push(
+                    <GridActionsCellItem
+                        key="edit"
+                        label="Edit"
+                        icon={<EditIcon />}
+                        onClick={() => editAction(params.row)}
+                        showInMenu
+                    />
+                );
+            }
+
+            if (deleteAction) {
+                actions.push(
+                    <GridActionsCellItem
+                        key="delete"
+                        label="Delete"
+                        icon={<DeleteIcon />}
+                        onClick={() => deleteAction(params.row)}
+                        showInMenu
+                    />
+                );
+            }
+            return actions;
+        }
+    };
+    columns.unshift(actionsColumn);
+
     return columns;
 };
