7fc8a0824b72f1345d516101ae7ae1d6197b982e
Jack Pflaum <jackpflaum@gmail.com>
2024-09-05 20:27:48 +0800

    update password backend logic

--- node-backend/controllers/UserController.ts
+++ node-backend/controllers/UserController.ts
@@ -27,5 +27,35 @@ export class UserController {
             console.log('updateUser() Error:', error);
             return res.status(500).json({ 'error': 'Internal server erorr' });
         }
-    }
-}
+    };
+
+
+    // update user password
+    static async updatePassword(req: Request, res: Response) {
+        // get id from url
+        // get password payload
+        const { id } = req.params;
+        const { data: password } = req.body;
+
+        if (!password) {
+            return res.status(400).json({ error: 'Password is required' });
+        };
+
+        try {
+            // TODO: 
+            const user = await User.findOne({ where: { id: id } });
+
+            if (!user) {
+                return res.status(404).json({ error: 'User not found' });
+            };
+
+            // update password (password is hashed on update)
+            user.password = password
+            await user.save();
+
+            return res.status(200).json({ success: "User password has been updated." });
+        } catch (error) {
+            return res.status(500).json({ error: 'Internal server error' });
+        };
+    };
+};
