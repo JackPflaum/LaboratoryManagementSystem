7e54d271876d0edcfd4857e69cde959922a8ecf6
Jack Pflaum <jackpflaum@gmail.com>
2024-09-08 14:28:49 +0800

    added input fields, validation and mutations

--- react-frontend/src/components/features/dialogs/profile-dialog.tsx
+++ react-frontend/src/components/features/dialogs/profile-dialog.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Stack, Typography } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Stack, TextField, Typography } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { ProfileAttributes } from "../../../types/interfaces";
 
 
 // user/profile details validation schema
 const ProfileDialogSchema = yup.object().shape({
-
+    personalEmail: yup.string().trim().required("Email is required.").matches(/\S+@\S+\.\S/, "Invalid email address"),
+    phoneNumber: yup.string().optional(),
 });
 
 interface ProfileDialogProps {
@@ -20,14 +22,34 @@ const ProfileDialog = ({ data, open, handleClose }: ProfileDialogProps) => {
     // set error messages when submitting form
     const [error, setError] = useState<string>("");
 
-    const mapDataToForm = (data: ProfileAttributes) => {
-        // TODO: map data
+    const mapDataToForm = (data?: ProfileAttributes) => {
+        return {
+            personalEmail: data?.personalEmail ?? "",
+            phoneNumber: data?.phoneNumber ?? "",
+        };
     };
 
-    const { handleSubmit, control, formState: { errors } } = useForm<ProfileAttributes>({
+    const { handleSubmit, control, reset, formState: { errors } } = useForm<ProfileAttributes>({
         defaultValues: mapDataToForm(data),
         resolver: yupResolver(ProfileDialogSchema),
     });
+
+    const { mutate: updateProfile } = useUpdateProfileMutation();
+
+    // handle profile form submission
+    const onSubmit = (formData: ProfileAttributes) => {
+
+        // update profile in database
+        updateProfile({ data: formData, id: data.id }, {
+            onSuccess: () => {
+                reset();
+                handleClose();
+            },
+            onError: (error: Error) => {
+                setError(error.message);
+            }
+        });
+    };
 
     return (
         <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
@@ -35,7 +57,38 @@ const ProfileDialog = ({ data, open, handleClose }: ProfileDialogProps) => {
             <Divider />
             <DialogContent>
                 <Stack spacing={2}>
-
+                    <Controller
+                        name="personalEmail"
+                        control={control}
+                        render={({ field, fieldState }) => (
+                            <TextField
+                                label="Personal Email"
+                                value={field.value}
+                                onChange={(e) => {
+                                    field.onChange(e)
+                                }}
+                                error={!!fieldState.error}
+                                helperText={fieldState.error?.message}
+                                size="small"
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="phoneNumber"
+                        control={control}
+                        render={({ field, fieldState }) => (
+                            <TextField
+                                label="Phone Number"
+                                value={field.value}
+                                onChange={(e) => {
+                                    field.onChange(e)
+                                }}
+                                error={!!fieldState.error}
+                                helperText={fieldState.error?.message}
+                                size="small"
+                            />
+                        )}
+                    />
                 </Stack>
             </DialogContent>
             <DialogActions>
