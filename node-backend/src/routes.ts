794c0cbf77a7bf0a9c342d3e5cd581f9fd9941fe
Jack Pflaum <jackpflaum@gmail.com>
2024-09-09 20:22:22 +0800

    added delete routes

--- node-backend/src/routes.ts
+++ node-backend/src/routes.ts
@@ -11,7 +11,9 @@ import { AdminController } from '../controllers/AdminController';
 const router = Router();
 
 // Admin related requests
+router.post('/admin', AdminController.getUsers);
 router.post('/admin/add-new-user', AdminController.addNewUser);
+router.delete('/admin/delete-user/:id', AdminController.deleteUser);
 
 // Dashboard related requests 
 router.get('/dashboard', DashboardController.getDashboard);
@@ -21,6 +23,7 @@ router.get('/clients', ClientController.getClients);
 router.get('/clients/:id', ClientController.getClientDetails);
 router.post('/clients/add-new-client', ClientController.addNewClient);
 router.put('/clients/:id/update-client-details', ClientController.updateClientDetails);
+router.delete('/clients/:id/delete', ClientController.deleteClient);
 router.get('/clients/confirm-client-exists', ClientController.confirmClientExists);
 
 // Job related requests
@@ -28,11 +31,14 @@ router.get('/jobs', JobController.getJobs);
 router.get('/jobs/:id', JobController.getJobDetails);
 router.post('/jobs/add-new-job', JobController.addNewJob);
 router.put('/jobs/:id/update-job-details', JobController.updateJobDetails);
+router.get('/jobs/:id/delete', JobController.deleteJob);
 
 // Sample related requests
-router.get('/sample-details/:id', SampleController.getSampleDetails);
-router.put('/sample-details/:id/update-sample-details', SampleController.updateSampleDetails);
-router.delete('/sample-details/:id/delete-sample', SampleController.deleteSample);
+router.get('/samples', SampleController.getSamples);
+router.get('/sample/:id', SampleController.getSampleDetails);
+router.post('/sample/add-new-sample', SampleController.addNewSample);
+router.put('/sample/:id/update-sample-details', SampleController.updateSampleDetails);
+router.delete('/sample/:id/delete-sample', SampleController.deleteSample);
 
 // User Authorization requests
 router.post('/user-login', AuthorizationController.userlogin);
@@ -46,4 +52,3 @@ router.put('/user/:id/update-user', UserController.updateUser);
 router.put('/user/:id/update-password', UserController.updatePassword);
 
 export default router;
-
