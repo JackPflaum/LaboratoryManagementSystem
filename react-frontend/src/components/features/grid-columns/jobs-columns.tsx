ff7b87621239ba73c41278ee319c46ad85e0fb6f
Jack Pflaum <jackpflaum@gmail.com>
2024-09-14 16:35:47 +0800

    added edit and delete actions

--- react-frontend/src/components/features/grid-columns/jobs-columns.tsx
+++ react-frontend/src/components/features/grid-columns/jobs-columns.tsx
@@ -2,30 +2,25 @@ import { Typography } from "@mui/material";
 import {
     GridColDef,
     GridRowParams,
-    GridRowId,
     GridActionsCellItem,
     GridRenderCellParams
 } from "@mui/x-data-grid";
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import CompletedChip from "../completed-chip";
 import IncompleteChip from "../incomplete-chip";
+import { JobAttributes } from "../../../types/interfaces";
 
 
-export function getJobsColumns(useViewJob: (id: GridRowId) => void): GridColDef[] {
-    return [
-        {
-            field: "actions",
-            type: "actions",
-            getActions: (params: GridRowParams) => [
-                <GridActionsCellItem
-                    key="view-action"
-                    label="View"
-                    icon={<VisibilityIcon />}
-                    showInMenu
-                    onClick={() => useViewJob(params.id)}
-                />
-            ],
-        },
+interface JobColumnProps {
+    viewAction: (row: JobAttributes) => void;
+    editAction?: (row: JobAttributes) => void;
+    deleteAction?: (row: JobAttributes) => void;
+};
+
+export function getJobsColumns({ viewAction, editAction, deleteAction }: JobColumnProps) {
+    const columns: GridColDef[] = [
         {
             field: "jobNumber",
             headerName: "Job Number",
@@ -62,4 +57,50 @@ export function getJobsColumns(useViewJob: (id: GridRowId) => void): GridColDef[
             ),
         },
     ];
+
+    const actionsColumn: GridColDef = {
+        field: "actions",
+        type: "actions",
+        width: 100,
+        getActions: (params: GridRowParams<JobAttributes>) => {
+            const actions = [
+                <GridActionsCellItem
+                    key="view"
+                    label="View"
+                    icon={<VisibilityIcon />}
+                    onClick={() => viewAction(params.row)}
+                    showInMenu
+                />
+            ];
+
+            if (editAction) {
+                actions.push(
+                    <GridActionsCellItem
+                        key="edit"
+                        label="Edit"
+                        icon={<EditIcon />}
+                        onClick={() => editAction(params.row)}
+                        showInMenu
+                    />
+                );
+            };
+
+            if (deleteAction) {
+                actions.push(
+                    <GridActionsCellItem
+                        key="delete"
+                        label="Delete"
+                        icon={<DeleteIcon />}
+                        onClick={() => deleteAction(params.row)}
+                        showInMenu
+                    />
+                );
+            }
+            return actions;
+        }
+    };
+
+    columns.unshift(actionsColumn);
+
+    return columns;
 };
