99b067b9bd1606a8c530a93df02710a6c7ecca41
Jack Pflaum <jackpflaum@gmail.com>
2024-09-16 20:52:12 +0800

    added column actions

--- react-frontend/src/components/IndividualJob.tsx
+++ react-frontend/src/components/IndividualJob.tsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import { Typography, Box } from "@mui/material";
 import DisplayGrid from "./features/display-grid";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ClientToolbar from "./features/client-toolbar";
 import EditIcon from '@mui/icons-material/Edit';
-import { useGetJobQuery } from "../queries/useQueries";
+import { useGetJobQuery, useGetSamplesQuery } from "../queries/useQueries";
 import JobDialog from "./features/dialogs/job-dialog";
+import { getSamplesColumns } from "./features/grid-columns/samples-columns";
+import { UserPermissions } from "../types/enums";
+import { useHasPermission } from "../hooks/custom-hooks";
+import { JobAttributes, SampleAttributes } from "../types/interfaces";
 
 
 const IndividualJob = () => {
 
     const [searchFilter, setSearchFilter] = useState<string>("");
     const [openDialog, setOpenDialog] = useState<boolean>(false);
+    const [editingJob, setEditingJob] = useState<JobAttributes | undefined>(undefined);
+    const [openDeleteDialog, setOpenDeleteDialog] = useState<boolean>(false);
 
     const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchFilter(event.target.value);
@@ -25,7 +31,29 @@ const IndividualJob = () => {
     // get list of samples for currently selected Job
     const { data: samplesData, isLoading } = useGetSamplesQuery(searchFilter);
 
-    const columns = getSamplesColumns();
+    const navigate = useNavigate();
+
+    // view selected sample on dedicated page
+    const viewAction = (row: SampleAttributes) => {
+        navigate(`/samples/${row.id}`);
+    };
+
+    // edit existing sample
+    const editAction = (row: SampleAttributes) => {
+        // setEditingSamples(row);
+        setOpenDialog(true);
+    };
+
+    // delete existing sample
+    const deleteAction = (row: SampleAttributes) => {
+        setOpenDeleteDialog(true);
+    };
+
+    const columns = getSamplesColumns(useHasPermission(UserPermissions.ADD_EDIT_JOBS) ? {
+        viewAction,
+        editAction,
+        deleteAction
+    } : { viewAction });
 
     return (
         <>
@@ -52,7 +80,7 @@ const IndividualJob = () => {
                 columns={columns}
                 isLoading={isLoading}
             />
-            {openDialog && <JobDialog open={openDialog} handleClose={() => setOpenDialog(false)} data={jobData} />}
+            {openDialog && <JobDialog open={openDialog} handleClose={() => setOpenDialog(false)} data={editingJob} />}
         </>
     );
 };
