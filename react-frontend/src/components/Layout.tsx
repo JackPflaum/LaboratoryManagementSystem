bb9013a55c010db4496e71d3e6e58dd1f40bf6f1
Jack Pflaum <jackpflaum@gmail.com>
2024-11-18 20:22:52 +0800

    complete navigation bar change because previous code was causing complete page refresh on click of links.

--- react-frontend/src/components/Layout.tsx
+++ react-frontend/src/components/Layout.tsx
@@ -1,9 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+import { useAuthUser } from "../context/UserAuthContext";
+import { AdminContextAttributes, UserContextAttributes } from "../types/interfaces";
 import NavigationSidebar from "./NavigationSidebar";
 
-const Layout = () => {
+interface LayoutProps {
+    user?: UserContextAttributes | null,
+};
+
+const Layout = ({ user }: LayoutProps) => {
+
     return (
-        <NavigationSidebar />
-    )
+        <NavigationSidebar user={user} />
+    );
 };
 
 export default Layout;
--- react-frontend/src/components/NavigationSidebar.tsx
+++ react-frontend/src/components/NavigationSidebar.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import BiotechIcon from '@mui/icons-material/Biotech';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ScienceIcon from '@mui/icons-material/Science';
@@ -10,59 +10,119 @@ import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { AppProvider } from "@toolpad/core";
 import type { Navigation } from '@toolpad/core';
 import { darkTheme, lightTheme } from "../themes/theme";
+import { UserContextAttributes } from "../types/interfaces";
+import { useLogoutMutation } from "../queries/useQueries";
+import { AppBar, Box, Button, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
 
-const NavigationSidebar = () => {
+interface NavigationProps {
+    user?: UserContextAttributes | null,
+}
 
-    const { user, isLoading } = useAuthUser();
+const NavigationSidebar = ({ user }: NavigationProps) => {
 
-    const navigationLinks: Navigation = [
-        {
-            kind: "header",
-            title: "Main Items",
-        },
+    const navigationLinks = [
         {
             title: "Dashboard",
             icon: <DashboardIcon />,
-            segment: "dashboard"
+            to: "dashboard"
         },
         {
             title: "Jobs",
             icon: <ScienceIcon />,
-            segment: "jobs"
+            to: "jobs"
         },
 
         {
             title: "Clients",
             icon: <BusinessIcon />,
-            segment: "clients"
+            to: "clients"
+        },
+    ];
+
+    const userLinks = [
+        {
+            title: "User",
+            icon: <PersonIcon />,
+            // to: `user/${user.id}`,
+            to: "/"
         },
         {
-            kind: "divider",
+            title: "Logout",
+            icon: <LogoutIcon />,
+            to: "/"
         }
     ];
 
-    if (user) {
-        console.log("USER:", user);
-        navigationLinks.push({
-            title: "User",
-            icon: <PersonIcon />,
-            segment: `user/${user.id}`,
-        })
+    const navigate = useNavigate();
+
+    const { mutate: logoutUser } = useLogoutMutation();
+
+    const handleLogout = () => {
+        logoutUser();
+        navigate("/user-login");
     };
 
+    const drawerWidth = 240;
+
     return (
-        <AppProvider
-            navigation={navigationLinks}
-            branding={{
-                // logo: <BiotechIcon />,
-                title: "Laboratory Management System"
-            }}
-            theme={{ light: lightTheme, dark: darkTheme }}
-        >
-            <DashboardLayout disableCollapsibleSidebar>
+        <Box sx={{ display: "flex" }}>
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                <Toolbar>
+                    <Typography variant="h6" noWrap component="div">
+                        Laboratory Management System
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]:
+                    {
+                        width: drawerWidth,
+                        boxSizing: "border-box"
+                    },
+                }}>
+                <Toolbar />
+                <Box sx={{ overflow: "auto", padding: 2 }}>
+                    <List>
+                        {navigationLinks.map((item) => (
+                            <ListItem key={item.title} component={Link} to={item.to}>
+                                <ListItemIcon>
+                                    {item.icon}
+                                </ListItemIcon>
+                                <ListItemText primary={item.title} />
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {userLinks.map((item) => (
+                            item.title === "Logout" ? (
+                                <ListItemButton key={item.title} onClick={handleLogout}>
+                                    <ListItemIcon>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.title} />
+                                </ListItemButton>
+                            ) : (
+                                <ListItem key={item.title} component={Link} to={item.to}>
+                                    <ListItemIcon>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.title} />
+                                </ListItem>
+                            )
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
+            <Box sx={{ flexGrow: 1, p: 3 }}>
+                <Toolbar />
                 <Outlet />
-            </DashboardLayout>
-        </AppProvider>
+            </Box>
+        </Box>
 
     )
 };
