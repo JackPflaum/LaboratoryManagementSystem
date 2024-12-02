296502ba58bb18c3885a2a17040e6941b9726a73
Jack Pflaum <jackpflaum@gmail.com>
2024-12-02 19:48:57 +0800

    addNewSample now creates new Samples and Tests.

--- node-backend/controllers/SampleController.ts
+++ node-backend/controllers/SampleController.ts
@@ -3,6 +3,9 @@ import Sample from '../src/database/models/Sample';
 import { Op } from 'sequelize';
 import { SampleAttributes } from '../src/database/types/models-interface';
 import Job from '../src/database/models/Job';
+import sequelize from '../src/database/models/db';
+import Test from '../src/database/models/Test';
+import { incrementSampleNumber } from '../src/functions/miscellaneousFunctions';
 
 // handles requests related to samples
 export class SampleController {
@@ -11,6 +14,7 @@ export class SampleController {
     static async getSamples(req: Request, res: Response) {
         const searchFilter = req.query.search as string;
         const jobId = req.query.jobId as string;
+        const userId = req.query.userId as string;
 
         const whereCondition: any = {};
 
@@ -29,7 +33,16 @@ export class SampleController {
 
         try {
             const samples = await Sample.findAll({
-                where: whereCondition
+                where: whereCondition,
+                include: [
+                    {
+                        model: Test,
+                        as: "tests",
+                        // attributes: ['id'],
+                        attributes: { exclude: ['testId'] },
+                        where: userId ? { userId: userId } : {},
+                    }
+                ]
             });
 
             // respond with retieved sample data
@@ -59,7 +72,6 @@ export class SampleController {
     };
 
 
-    // add a new sample to the database
     static async addNewSample(req: Request, res: Response) {
         const {
             jobNumber,
@@ -67,20 +79,45 @@ export class SampleController {
             storage,
             completed,
             comments,
-            numberOfSamples }: SampleAttributes = req.body;
+            numberOfSamples,
+            tests }: SampleAttributes & { tests: { userId: number, testName: string, unit: string }[] } = req.body;
+
         try {
+            // wrap database operations in a single transaction, which automatically rolls back if anything fails
+            await sequelize.transaction(async (t) => {
+                if (numberOfSamples) {
+                    // Iterate through the number of samples and create them one by one
+                    for (let i = 0; i < numberOfSamples; i++) {
+                        // Fetch the most recent sample number for the given jobNumber
+                        const lastSample = await Sample.findOne({
+                            where: { jobNumber },
+                            order: [['sampleNumber', 'DESC']],
+                            transaction: t
+                        });
 
-            if (numberOfSamples) {
-                for (let i = 0; i <= numberOfSamples; i++) {
-                    // create a new job number incremented up from most recently created job
-                    const sampleNumber = await Sample.createSampleNumber(jobNumber);
+                        // Increment the sample number from the last sample number
+                        let newSampleNumber = incrementSampleNumber(lastSample, jobNumber);
 
-                    await Sample.create({ jobNumber, sampleNumber, type, storage, completed, comments });
-                };
-            };
+                        // Create the new sample with the incremented sample number
+                        const createdSample = await Sample.create(
+                            { jobNumber, sampleNumber: newSampleNumber, type, storage, completed, comments },
+                            { transaction: t }
+                        );
+
+                        // Create tests for corresponding sample
+                        for (let test of tests) {
+                            await Test.create(
+                                { sampleId: createdSample.id, userId: test.userId, testName: test.testName, unit: test.unit },
+                                { transaction: t }
+                            );
+                        };
+                    }
+                }
+            });
 
             return res.status(201).json({ success: "New Sample has been created" });
         } catch (error) {
+            console.log("ERROR: ", error);
             return res.status(500).json({ error: 'Internal server error.' });
         };
     };
@@ -93,7 +130,8 @@ export class SampleController {
             type,
             storage,
             completed,
-            comments }: SampleAttributes = req.body;
+            comments,
+            tests }: SampleAttributes & { tests: { id: number, userId: number, testName: string, unit: string }[] } = req.body;
         try {
             const sample = await Sample.findByPk(sampleId);
 
@@ -108,11 +146,17 @@ export class SampleController {
                 comments: comments
             });
 
+            // update tests
+            // TODO: update both samples and tests in one transaction?
+            for (let test of tests) {
+                await Test.update({ id: id, sampleId: sample.id, userId: test.userId, testName: test.testName, unit: test.unit },)
+            };
+
             return res.status(201).json({ success: "Sample Details updated" });
         } catch (error) {
             return res.status(500).json({ error: 'Internal server error.' });
-        }
-    }
+        };
+    };
 
 
     // handle deleting Sample
@@ -126,10 +170,16 @@ export class SampleController {
                 return res.status(404).json({ error: 'Sample not found' });
             };
 
+            // Delete associated tests manually
+            await Test.destroy({
+                where: { sampleId: sampleId }
+            });
+
             await sample.destroy();
 
             return res.status(200).json({ success: "Sample deleted successfully" });
         } catch (error) {
+            console.log("ERROR---->", error);
             return res.status(500).json({ error: 'Internal server error' });
         };
     };
