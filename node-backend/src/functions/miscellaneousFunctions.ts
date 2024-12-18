04780556bc147931b0224ecae340f81fd0f830ac
Jack Pflaum <jackpflaum@gmail.com>
2024-12-18 21:06:39 +0800

    created samplesCompleted function

--- node-backend/src/functions/miscellaneousFunctions.ts
+++ node-backend/src/functions/miscellaneousFunctions.ts
@@ -1,7 +1,9 @@
+import Sample from "../database/models/Sample";
+import Test from "../database/models/Test";
 import { SampleAttributes } from "../database/types/models-interface";
 
 
-export const incrementSampleNumber = (jobNumber: string, sample?: SampleAttributes) => {
+export const incrementSampleNumber = (jobNumber: string, sample: SampleAttributes | null) => {
     // Increment the sample number from the previous ("2024-1-001" becomes "2024-1-002") or create a new one.
     if (sample?.sampleNumber) {
         // The last sample number (e.g., "2024-1-001")
@@ -18,4 +20,32 @@ export const incrementSampleNumber = (jobNumber: string, sample?: SampleAttribut
         // If no previous sample exists, create the first sample number
         return `${jobNumber}-001`;
     };
+};
+
+
+// checks if all samples are completed based on whether tests have results.
+export async function samplesCompleted(jobNumber: string): Promise<boolean> {
+    // Fetch all samples and include the associated tests in the same query
+    const samples = await Sample.findAll({
+        where: { jobNumber: jobNumber },
+        include: [{
+            model: Test,
+            as: "tests",
+            attributes: ["result"]
+        }],
+    });
+
+    // iterate over all sample tests to see if results are not null.
+    for (let sample of samples) {
+        if (sample.tests) {
+            for (let test of sample.tests) {
+                if (test.result === null) {
+                    return false;   // return false immediately if any test has no result
+                };
+            };
+        };
+    };
+
+    // all tests have results if this point is reached
+    return true;
 };
